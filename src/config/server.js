7aabd1ed6529a5e641a51edd089283c27ac6453f
maxidiflumeri <maxidiflumeri@gmail.com>
2020-08-02 18:37:37 -0300

    .

--- src/config/server.js
+++ src/config/server.js
@@ -2,12 +2,12 @@ import express from 'express'
 import router from '../routes/index.js'
 import cors from 'cors'
 import morgan from 'morgan'
-// import connect from 'connect-history-api-fallback'
+import connect from 'connect-history-api-fallback'
 
 const main = () =>{
     const app = express()
 
-    // app.use(connect())
+    app.use(connect())
     app.use(express.json())
     app.use(cors())
     app.use(morgan('dev'))
